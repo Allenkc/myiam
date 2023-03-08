569ce5b42982fd0f7dcf2114538b1d88f59e74ea
AllenKuo <kkc@cht.com.tw>
2023-03-08 13:48:57 +0800

    Initial commit

---
+++ src/main/java/tw/kkc/myiam/MyiamApplication.java
@@ -0,0 +1,13 @@
+package tw.kkc.myiam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyiamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyiamApplication.class, args);
+	}
+
+}
