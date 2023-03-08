fa6232d09472a5a90149dc9f4bcb43a66a5544ae
AllenKuo <kkc@cht.com.tw>
2023-03-08 17:28:44 +0800

    [add] Security Configuration class to set-up the authentication
    1. still need to add @Configuration not only @EnableSecurity or the config will not work properly
    2. current only expose /api/hello for demo usage

---
+++ src/main/java/tw/kkc/myiam/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package tw.kkc.myiam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
+
+/**
+ * This is a Configuration class for Spring security , note that the @Configuration need to add
+ * to this class or the configuration will not work properly
+ */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .csrf()
+                .disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests(
+                        (requests) -> requests.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                .requestMatchers("/api/hello").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic();
+        // @formatter:on
+        return http.build();
+    }
+}
